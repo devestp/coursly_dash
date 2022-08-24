19c144b95c846f3d7e1a62ae07dcce783f472a63
sina <im.devestp@gmail.com>
2022-08-24 20:11:52 +0430

    chore: register vue router and routes in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,13 @@
 import { createApp } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router';
 import './style.css'
 import App from './App.vue'
+import { routesDef } from './routes';
 
-createApp(App).mount('#app')
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: routesDef,
+});
+
+createApp(App).use(router).mount('#app')
